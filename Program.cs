644915fc9738cb395b45cd5faf79546c05359b00
Maksim229 <79194158+Maksim229@users.noreply.github.com>
2021-03-19 16:36:05 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,125 +1,122 @@
-﻿////Задание 1 Слова в обратом порядке
-//using System;
-
-
-//namespace Лаба_1
-//{
-//    class Program
-//    {
-//        static int Main(string[] args)
-//        {
-//        input_point:
-//            Console.WriteLine("Введите строку");
-//            string s = Console.ReadLine();
-//            char[] buf = s.ToCharArray();
-//            int sizebuf = s.Length;
-//            char[] str = new char[sizebuf + 2];
-//            str[0] = ' ';
-//            for (int i = 0; i < sizebuf; i++)
-//            {
-//                str[i + 1] = buf[i];
-//            }
-//            int size = str.Length;
-//            int finich = size;
-//            for (int i = size - 1; i >= 0; i--)
-//            {
-//                if (str[i] == ' ')
-//                {
-//                    for (int j = i + 1; j < finich; j++)
-//                    {
-//                        Console.Write(str[j]);
-//                    }
-//                    Console.Write(" ");
-//                    finich = i;
-//                }
-//            }
-//            Console.WriteLine("\n Если хотите повторить , нажмите y");
-//            string a=Console.ReadLine();
-//            if (a[0] == 'y')
-//                goto input_point;
-//            else
-//                return 0;
-
-
-
-//        }
-//    }
-//}
-
-//// Задание 2 Все буквы z после гласных менять на a
-//using System;
-
-//namespace Лаба_1
-//{
-//    class Program
-//    {
-//        static int Main(string[] args)
-//        {
-//            input_point:
-//            Console.WriteLine("Введите строку");
-//            string s = Console.ReadLine();
-//            char[] str = s.ToCharArray();
-//            int size = str.Length;
-//            char[] bufarray = new char[] { 'a', 'e', 'y', 'u', 'i', 'O' };
-//            for (int i = 0; i < size; i++)
-//            {
-//                if (str[i] == 'z')
-//                {
-//                    for (int j = 0; j < 5; j++)
-//                    {
-//                        if (str[i - 1] == bufarray[j])
-//                        {
-//                            str[i] = 'a';
-//                        }
-//                    }
-//                }
-//            }
-//            Console.Write(str);
-//            Console.WriteLine("\n Если хотите повторить , нажмите y");
-//            string a = Console.ReadLine();
-//            if (a[0] == 'y')
-//                goto input_point;
-//            else
-//                return 0;
-//        }
-//    }
-//}
-
-
-////Задание 3 Вывести на экран рандомные 30 символов строки состоящей из 256 символов 
-//using System;
-
-//namespace Лаба_1
-//{
-//    class Program
-//    {
-//        static int getrandomnumber()
-//        {
-//            Random rnd = new Random();
-//            int number = rnd.Next(0, 9);
-//            return number;
-//        }
-//        static char getrandomletter()
-//        {
-//            Random rnd = new Random();
-//            int number = rnd.Next(0, 26);
-//            char latter = (char)('a' + number);
-//            return latter;
-//        }
-//        static void Main(string[] args)
-//        {
-//            char[] str = new char[256];
-//            for (int i = 0; i < 256; i++)
-//            {
-//                str[i] = getrandomletter();
-//            }
-//            for (int i = 0; i < 30; i++)
-//            {
-//                Console.Write(str[getrandomnumber()]);
-//                Console.Write(" ");
-//            }
-
-//        }
-//    }
-//}
+﻿using System;
+namespace Лаба_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите выражение: ");
+                string str = Console.ReadLine();
+                int size = str.Length, sizen = str.Length / 2 + 1, sizes = str.Length / 2 + 1, j = 0, z = 0;
+                char[] arrays = new char[sizes];
+                int[] arrayn = new int[sizen];
+                bool arror = true;
+                int[] arraynumber = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                for(int i=0;i<size-1;i++)
+                {
+                    if (str[i] == '*' || str[i] == '/' || str[i] == '-' || str[i] == '+')
+                    {
+                        if (str[i+1] == '*' || str[i+1] == '/' || str[i+1] == '-' || str[i+1] == '+')
+                        {
+                            arror = false;
+                        }
+                    }
+                }
+                if (sizen > size / 2)
+                {
+                    arror = false;               
+                }
+                if (arror == true)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (str[i] == '*' || str[i] == '/' || str[i] == '-' || str[i] == '+')
+                        {
+                            arrays[j] = str[i]; j++;
+                        }
+                        else
+                        {
+                            arrayn[z] = Convert.ToInt32(str[i]) - 48; z++;
+                        }
+                    }
+                    int buf;
+                    for (int i = 0; i < sizen; i++)
+                    {
+                        if (arrays[i] == '^')
+                        {
+                            buf = Convert.ToInt32(Math.Pow(arrayn[i], arrayn[i + 1]));
+                            if (i > 0 && arrays[i - 1] == '-')
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '-';
+                            }
+                            else if (i > 0 && arrays[i - 1] == '+')
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '+';
+                            }
+                            else if (i == 0)
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '+';
+                            }
+                        }
+                        else if (arrays[i] == '*')
+                        {
+                            buf = arrayn[i] * arrayn[i + 1];
+                            if (i > 0 && arrays[i - 1] == '-')
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '-';
+                            }
+                            else if (i > 0 && arrays[i - 1] == '+')
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '+';
+                            }
+                            else if (i == 0)
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '+';
+                            }
+                        }
+                        else if (arrays[i] == '/')
+                        {
+                            buf = arrayn[i] / arrayn[i + 1];
+                            if (i > 0 && arrays[i - 1] == '-')
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '-';
+                            }
+                            else if (i > 0 && arrays[i - 1] == '+')
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '+';
+                            }
+                            else if (i == 0)
+                            {
+                                arrayn[i] = 0; arrayn[i + 1] = buf; arrays[i] = '+';
+                            }
+                        }
+                    }
+                    int result = arrayn[0];
+                    for (int i = 0; i < sizen; i++)
+                    {
+                        if (arrays[i] == '+')
+                        {
+                            result = result + arrayn[i + 1];
+                        }
+                        else if (arrays[i] == '-')
+                        {
+                            result = result - arrayn[i + 1];
+                        }
+                    }
+                    Console.WriteLine(result);
+                }
+                else
+                Console.WriteLine("В вашей записи есть ошибка ");
+                Console.WriteLine("Если хотите повторить,нажмите'y' ");
+                string repeat = Console.ReadLine();
+                if (repeat[0] != 'y')
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
 
