441a3b28ab808136d9875ff77346bea84cb5955c
Maksim229 <79194158+Maksim229@users.noreply.github.com>
2021-05-14 19:54:05 +0300

    Add files via upload

---
+++ Лаба 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Media;
+namespace Лаба_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer(@"Lab4.wav");
+            player.Play();
+            Console.ReadKey();
+        }
+    }
+}
+
